6a17de618f77bff2f1cfca1bc49e08482e89d0b0
Denty <dentymods.axuka@gmail.com>
2022-09-27 21:11:43 -0400

    int plugin

---
+++ DisableStuffPlugin/EventHandler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Exiled.API;
+using Exiled.API.Features;
+
+using System.Threading.Tasks;
+using Player = Exiled.API.Features.Player;
+using Server = Exiled.API.Features.Server;
+using Exiled.Events.EventArgs;
+
+namespace DisableStuffPlugin
+{
+    public class EventHandler
+    {
+        public void OnRoundStart()
+        {
+            Log.Debug("round started");
+        }
+    }
+}
+
---
+++ DisableStuffPlugin/Plugin.cs
@@ -0,0 +1,38 @@
+﻿using Exiled.API.Features;
+using System;
+using PlayerEvent = Exiled.Events.Handlers.Player;
+using ServerEvent = Exiled.Events.Handlers.Server;
+
+namespace DisableStuffPlugin
+{
+
+    public class DisableStuffPlugin : Plugin<Config>
+    {
+        public EventHandler EventHandler;
+        public static DisableStuffPlugin Singleton;
+
+        public override string Name { get; } = "DisableStuffPlugin";
+        public override string Author { get; } = "Denty";
+        public override string Prefix { get; } = "DSP"; //DisableStuffPlugin
+        public override Version Version { get; } = new Version(1, 0, 0);
+        public override Version RequiredExiledVersion { get; } = new Version(5, 3, 0);
+
+        public override void OnEnabled()
+        {
+            Singleton = this;
+            EventHandler = new EventHandler();
+
+            //EnableEvents
+            ServerEvent.RoundStarted += EventHandler.OnRoundStart;
+        }
+
+        public override void OnDisabled()
+        {
+            //DisableEvents
+            ServerEvent.RoundStarted -= EventHandler.OnRoundStart;
+
+            Singleton = null;
+            base.OnDisabled();
+        }
+    }
+}
